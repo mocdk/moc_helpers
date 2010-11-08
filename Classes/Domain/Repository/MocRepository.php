141a5949352ff76fe1d5b1d1004c78d7071a67bb
www-data <www-data@faraday.mocsystems.com>
2010-11-08 22:05:54 +0100

    up

--- Classes/Domain/Repository/MocRepository.php
+++ Classes/Domain/Repository/MocRepository.php
@@ -1,5 +1,11 @@
 <?php
 class Tx_MocHelpers_Domain_Repository_MocRepository extends Tx_Extbase_Persistence_Repository{
+	public function createQuery(){
+		$query = parent::createQuery();
+		$query->getQuerySettings()->setRespectStoragePage(FALSE);
+		return $query;
+	}
+
 	 /**
 	 * @param array $uids array of ids
 	 */
@@ -12,11 +18,71 @@ class Tx_MocHelpers_Domain_Repository_MocRepository extends Tx_Extbase_Persisten
 		return $this->query->matching($criterion)->execute();
 	}
 
+	public function findBy($field, $value) {
+		$Query = $this->createQuery();
+		$Criterion = $Query->equals($field, $value);
+		return $Query->matching($Criterion)->execute();
+	}
+
+	public function countBy($field, $value) {
+		$Query = $this->createQuery();
+		$Criterion = $Query->equals($field, $value);
+		return $Query->matching($Criterion)->count();
+	}
+
+	public function findOneBy($field, $value) {
+		$query = $this->createQuery();
+		$result = $query->matching($query->equals($field, $value))
+    		->setLimit(1)
+    		->execute();
+		$object = NULL;
+		if (count($result) > 0) {
+    		$object = current($result);
+		}
+		return $object;
+	}
+
 	public function save($object){
-		if(!$this->identityMap->hasObject($object)){
+		if (!$this->hasObject($object)) {
 			$this->add($object);
 		}
 		$this->saveAll();
+	}
+
+	public function hasObject($Object) {
+		if (!($Object instanceof $this->objectType)) {
+			throw new Tx_Extbase_Persistence_Exception_IllegalObjectType('The object given to update() was not of the type (' . $this->objectType . ') this repository manages.', 1249479625);
+		}
+
+		return $this->identityMap->hasObject($Object);
+	}
+
+	public function insertOrUpdate($Object, $field = 'uid') {
+		if (!($Object instanceof $this->objectType)) {
+			throw new Tx_Extbase_Persistence_Exception_IllegalObjectType('The object given to update() was not of the type (' . $this->objectType . ') this repository manages.', 1249479625);
+		}
+
+		$value = $Object->_getProperty($field);
+		if (!empty($value) && $this->countBy($field, $value) === 1) {
+			$OldObject = $this->findBy($field, $value);
+			$Object->setUid($Object->getUid());
+			$this->replace($OldObject, $Object);
+		} else {
+			$this->save($Object);
+		}
+	}
+
+	public function exists($Object) {
+		if (!($Object instanceof $this->objectType)) {
+			throw new Tx_Extbase_Persistence_Exception_IllegalObjectType('The object given to update() was not of the type (' . $this->objectType . ') this repository manages.', 1249479625);
+		}
+
+		$uid = $Object->getUid();
+		if (empty($uid)) {
+			throw new Tx_Extbase_Persistence_Exception_UnknownObject('The "object" is does not have an existing counterpart in this repository.', 1249479819);
+		}
+
+		return 1 === $this->countByUid($uid);
 	}
 
 	/**
@@ -29,17 +95,17 @@ class Tx_MocHelpers_Domain_Repository_MocRepository extends Tx_Extbase_Persisten
 	public function saveAll(){
 		$this->persistenceManager->persistAll();
 	}
+
 	public function logicalOrs($queries) {
 		if(!$this->query) {
-			$this->quuery = $this->createQuery();	
+			$this->quuery = $this->createQuery();
 		}
 		$combined_ors = array_pop($queries);
 		if (count($queries)) {
 			foreach ($queries as $criterion) {
 				$combined_ors = $this->query->logicalOr($criterion,$combined_ors);
 			}
-		}		
+		}
 		return $combined_ors;
 	}
-	
 }
