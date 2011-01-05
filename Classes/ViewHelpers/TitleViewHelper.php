2824649b5d3ce963ba129c52baf45778d66de5c0
www-data <www-data@dyson.mocsystems.com>
2011-01-05 08:39:25 +0100

    Changed title viewhelper

--- Classes/ViewHelpers/TitleViewHelper.php
+++ Classes/ViewHelpers/TitleViewHelper.php
@@ -10,15 +10,13 @@
  */
 class Tx_MocHelpers_ViewHelpers_TitleViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
 
-
 	/**
-	 * @param unknown_type $notPageTitle
-	 * @param unknown_type $notIndexedTitle
-	 * @api
+	 * @param boolean $notPageTitle
+	 * @param boolean $notIndexedTitle
 	 */
-	public function render($onlyDocumentTitle=false,$onlyIndexedTitle=false) {
+	public function render($onlyDocumentTitle = false, $onlyIndexedTitle = false) {
 		$title = $this->renderChildren();
-		
+
 		if(!$onlyIndexedTitle) {
 			$GLOBALS['TSFE']->altPageTitle = $title;
 		}
@@ -26,6 +24,5 @@ class Tx_MocHelpers_ViewHelpers_TitleViewHelper extends Tx_Fluid_Core_ViewHelper
 			$GLOBALS['TSFE']->indexedDocTitle = $title;
 		}
 	}
-}
 
-?>
+}
