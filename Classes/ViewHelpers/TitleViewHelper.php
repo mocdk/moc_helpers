11fc83dd4b6cd8f1640bbbd6dbbd2df9ff36f82a
www-data <www-data@dyson.mocsystems.com>
2011-04-06 08:57:06 +0200

    New title view helper

--- Classes/ViewHelpers/TitleViewHelper.php
+++ Classes/ViewHelpers/TitleViewHelper.php
@@ -1,32 +1,33 @@
 <?php
-/**
- * Title ViewHelper for fluid
- *
- * Example:
- * 		{namespace moc=Tx_MocHelpers_ViewHelpers}
- *
- *		  <moc:title>Title of Page and Indexing</moc:title>
- *		  <moc:title onlyIndexedTitle="1">Title of Page and Indexing</moc:title>
- */
 class Tx_MocHelpers_ViewHelpers_TitleViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
 
 	/**
+	 * @param string $title
 	 * @param boolean $notPageTitle
 	 * @param boolean $notIndexedTitle
+	 * @param boolean $prependPageTitle
 	 * @param boolean $appendPageTitle
+	 * @param string $splitString
 	 */
-	public function render($onlyDocumentTitle = false, $onlyIndexedTitle = false, $appendPageTitle = false) {
-		$title = $this->renderChildren();
-		
-		if($appendPageTitle) {
-			$title .= empty($GLOBALS['TSFE']->cObj->data['nav_title']) ? $GLOBALS['TSFE']->cObj->data['title'] : $GLOBALS['TSFE']->cObj->data['nav_title'];
+	public function render($title = '', $onlyDocumentTitle = false, $onlyIndexedTitle = false, $prependPageTitle = false, $appendPageTitle = false, $splitString = ' - ') {
+		if($title === '') {
+			$title = $this->renderChildren();
 		}
-
-		if(!$onlyIndexedTitle) {
-			$GLOBALS['TSFE']->altPageTitle = $title;
-		}
-		if(!$onlyDocumentTitle) {
-			$GLOBALS['TSFE']->indexedDocTitle = $title;
+		if(trim($title) !== '') {
+			if($prependPageTitle || $appendPageTitle) {
+				$originalTitle = empty($GLOBALS['TSFE']->cObj->data['nav_title']) ? $GLOBALS['TSFE']->cObj->data['title'] : $GLOBALS['TSFE']->cObj->data['nav_title'];
+				if($prependPageTitle) {
+					$title = $originalTitle.$splitString.$title;
+				} else {
+					$title .= $splitString.$originalTitle;
+				}
+			}
+			if(!$onlyIndexedTitle) {
+				$GLOBALS['TSFE']->altPageTitle = $title;
+			}
+			if(!$onlyDocumentTitle) {
+				$GLOBALS['TSFE']->indexedDocTitle = $title;
+			}
 		}
 	}
 
