741c5adaf93d8c208ced41be402e82bf538e6588
www-data <www-data@dyson.mocsystems.com>
2011-05-13 13:49:56 +0200

    Added new methods updateValuesOnObject that checks if an object already exists before creating a new one
    The old method 'convertDataToObject' just delegates to the new methods, and is declared deprecated

--- Classes/Domain/Model/Builder.php
+++ Classes/Domain/Model/Builder.php
@@ -4,11 +4,25 @@ class Tx_MocHelpers_Domain_Model_Builder {
 	/**
 	 * Convert a list (or all) properites from the step data into an object
 	 *
-	 * @param string $object The classname to add properties to
+	 * @param mixed $object The classname to add properties to
+	 * @param array $data The list of properties to add to the object
+	 * @return object
+	 * @deprecated 
+	 */
+	public static function convertDataToObject($object, $data = array()) {
+		return self::updateValuesOnObject($object, $data);
+	}
+	
+	/**
+	 * Convert a list (or all) properites from the step data into an object. If the object already exists, we just updates all properties from 
+	 * the data array.
+	 * 
+	 * 
+	 * @param mixed $object The classname to add properties to
 	 * @param array $data The list of properties to add to the object
 	 * @return object
 	 */
-	public static function convertDataToObject($object, $data = array()) {
+	public static function updateValuesOnObject($object, $data = array()) {
 		$objectManager = t3lib_div::makeInstance('Tx_Extbase_Object_ObjectManager');
 		$reflectionService = $objectManager->get('Tx_Extbase_Reflection_Service');
 		$dataMapper = $objectManager->get('Tx_Extbase_Persistence_Mapper_DataMapper');
@@ -30,7 +44,10 @@ class Tx_MocHelpers_Domain_Model_Builder {
 				$type = $propertyMetaData['type'];
 			}
 
-			if (class_exists($type)) {
+			if (class_exists($type) && !is_object($value)) {
+				if(isset($value['__identity'])) {
+					$value = $value['__identity'];
+				}
 				$result = $dataMapper->fetchRelated($object, $key, $value);
 				$value = $dataMapper->mapResultToPropertyValue($object, $key, $result);
 			}
