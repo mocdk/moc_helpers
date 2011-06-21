2764223b883e5e170e7763d06095f816bc52bbd0
www-data <www-data@mocsystems.com>
2011-06-21 23:29:35 +0200

    Improved upload helper.

--- Classes/UploadHelper.php
+++ Classes/UploadHelper.php
@@ -2,17 +2,31 @@
 class Tx_MocHelpers_UploadHelper {
 
 	/**
+	 * @var t3lib_basicFileFunctions
+	 */
+	protected $basicFileFunctions;
+
+	/**
+	 * @param t3lib_basicFileFunctions $basicFileFunctions
+	 * @return void
+	 */
+	public function injectBasicFileFunctions(t3lib_basicFileFunctions $basicFileFunctions) {
+		$this->basicFileFunctions = $basicFileFunctions;
+	}
+
+	/**
 	 * @param string $destinationFolder
 	 */
 	public function saveFilesToFolder($destinationFolder) {
+		$savedFiles = array();
 		try {
 			foreach($_FILES as $file) {
-				$this->copyFileToDestinationFolder($file, $destinationFolder);
+				array_push($savedFiles, $this->copyFileToDestinationFolder($file, $destinationFolder));
 			}
 		} catch(Exception $e) {
-			return $e->getMessage();
+			array_push($savedFiles, $e->getMessage());
 		}
-		return TRUE;
+		return $savedFiles;
 	}
 
 	/**
@@ -24,11 +38,22 @@ class Tx_MocHelpers_UploadHelper {
 		if ($error) {
 			throw new Exception($error);
 		}
+
+		$fileInfo['name'] = $this->basicFileFunctions->cleanFileName($fileInfo['name']);
+
+		if ($this->basicFileFunctions->checkFileNameLen($fileInfo['name']) === FALSE) {
+			throw new Exception(sprintf('File name too long: %s', $fileInfo['name']));
+		}
+
+		if ($fileInfo['size'] > (t3lib_div::getMaxUploadFileSize() * 1024)) {
+			throw new Exception(sprintf('File size too large: %s, %s', $fileInfo['name'], t3lib_div::formatSize($fileInfo['size'], ' bytes| KB| MB| GB')));
+		}
+
 		$filepath = $this->checkFilepath($destinationFolder . $fileInfo['name']);
 
 		t3lib_div::upload_copy_move($fileInfo['tmp_name'], PATH_site . $filepath);
 
-		return $filepath;
+		return array('path' => $filepath, 'name' => pathinfo($filepath, PATHINFO_BASENAME));
 	}
 
 	/**
