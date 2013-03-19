8476abc18393ec59a99b44ac7b79fbe1ca38493b
www-data <www-data@mocsystems.com>
2013-03-19 12:32:32 +0100

    [TASK] Use inject instead of makeInstance in flex form view helper
    Change-Id: I895597b6087bed59f1c7aec4cc4282aa8861d621

--- Classes/ViewHelpers/FlexFormViewHelper.php
+++ Classes/ViewHelpers/FlexFormViewHelper.php
@@ -2,11 +2,24 @@
 class Tx_MocHelpers_ViewHelpers_FlexFormViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
 
 	/**
+	 * @var Tx_Extbase_Service_FlexFormService
+	 */
+	protected $flexFormService;
+
+	/**
+	 * @param Tx_Extbase_Service_FlexFormService $flexFormService
+	 * @return void
+	 */
+	public function injectFlexFormService(Tx_Extbase_Service_FlexFormService $flexFormService) {
+		$this->flexFormService = $flexFormService;
+	}
+
+	/**
 	 * @param string $flexForm
 	 * @return array
 	 */
 	public function render($flexForm = '') {
-		return t3lib_div::makeInstance('Tx_Extbase_Service_FlexFormService')->convertFlexFormContentToArray($flexForm === '' ? $this->renderChildren() : $flexForm);
+		return $this->flexFormService->convertFlexFormContentToArray($flexForm === '' ? $this->renderChildren() : $flexForm);
 	}
 
 }
