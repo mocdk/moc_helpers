4a71f6df7465ef03826ed8116805c11cbf03fdbd
Aske Ertmann <aske@moc.net>
2013-04-03 17:00:00 +0200

    [TASK] Get rid of unnecessary function in flex form user func
    Change-Id: I4703eaff8566dc72bd3ac670fc107217886d5375

--- Classes/Userfunc/FlexFormHelper.php
+++ Classes/Userfunc/FlexFormHelper.php
@@ -6,14 +6,15 @@
  * 10 = USER
  * 10.userFunc = Tx_MocHelpers_Userfunc_FlexFormHelper->convert
  * 10.path = language
- * 10.field = pi_flexform (optional)
+ * 10.value = TEXT (optional)
+ * 10.value.field = page:pi_flexform
  */
 class Tx_MocHelpers_Userfunc_FlexFormHelper {
 
 	/**
 	 * @var tslib_cObj
 	 */
-	public $pObj;
+	public $cObj;
 
 	/**
 	 * @param string $content
@@ -22,43 +23,13 @@ class Tx_MocHelpers_Userfunc_FlexFormHelper {
 	 */
 	public function convert($content, array $configuration) {
 		$flexFormService = t3lib_div::makeInstance('Tx_Extbase_Service_FlexFormService');
-		$flexFormField = isset($configuration['field']) ? $configuration['field'] : 'pi_flexform';
-		$flexForm = $flexFormService->convertFlexFormContentToArray(isset($this->pObj->data[$flexFormField]) ? $this->pObj->data[$flexFormField] : '');
-		return isset($configuration['path']) ? Tx_Extbase_Utility_Arrays::getValueByPath($flexForm, $configuration['path']) : $flexForm;
-	}
-
-	/**
-	 * Convert the current page flex form data
-	 *
-	 * 10 = USER
-	 * 10.userFunc = Tx_MocHelpers_Userfunc_FlexFormHelper->convert
-	 * 10.configuration {
-	 *     tableField = pi_flexform
-	 *     flexFormField = language
-	 * }
-	 * 
-	 * @param string $content
-	 * @param array $configuration
-	 * @return mixed
-	 */
-	public function convertCurrentPageFlexForm($content, array $configuration) {
-
-		$tableField = $configuration['configuration.']['tableField'];
-		$flexFormField = $configuration['configuration.']['flexFormField'];
-
-		// Make sure that the field and flex form field is set before processing it
-		if ($tableField !== NULL && $flexFormField !== NULL) {
-
-			$flexFormService = t3lib_div::makeInstance('Tx_Extbase_Service_FlexFormService');
-			list($table, $uid) = t3lib_div::trimExplode(':', $GLOBALS['TSFE']->currentRecord, 1);
-			$record = $GLOBALS['TSFE']->sys_page->getRawRecord($table, $uid);
-			$flexFormXML = $record[$tableField];
-			$flexForm = $flexFormService->convertFlexFormContentToArray($flexFormXML);
-
-			return isset($flexForm[$flexFormField]) ? $flexForm[$flexFormField] : 0;
+		if (isset($configuration['value'])) {
+			$flexFormData = isset($configuration['value.']) ? $this->cObj->stdWrap($configuration['value'], $configuration['value.']) : $configuration['value'];
 		} else {
-			return;
+			$flexFormData = isset($this->cObj->data['pi_flexform']) ? $this->cObj->data['pi_flexform'] : '';
 		}
+		$flexForm = $flexFormService->convertFlexFormContentToArray($flexFormData);
+		return isset($configuration['path']) ? Tx_Extbase_Utility_Arrays::getValueByPath($flexForm, explode('.', $configuration['path'])) : $flexForm;
 	}
 
 }
