6563ea4a10f9708253f109a8b2eaa8e71c4f5272
Mattias Nilsson <mattias@moc.net>
2013-04-09 13:10:21 +0200

    [FEATURE] Add replace viewhelper
    Adds a replace viewhelper that uses preg_replace to
    find a certain pattern and process the value.
    
    Change-Id: I27e941d18ea38535c3c53e908c5cd6ce7915875f
    Refs: #11298

---
+++ Classes/ViewHelpers/ReplaceViewHelper.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Replace ViewHelper for fluid replace everything through a regex match
+ *
+ */
+class Tx_MocHelpers_ViewHelpers_ReplaceViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper   {
+
+	/**
+	 * Replace a value
+	 *
+	 * @param string $value The value to be processed
+	 * @param string $pattern A regex pattern
+	 * @param string $replace The replacement values
+	 * @return string $value
+	 */
+	public function render($value = '', $pattern = '', $replace = '') {
+
+		return preg_replace($pattern, $replace, $value === '' ? $this->renderChildren() : $value);
+	}
+
+}
