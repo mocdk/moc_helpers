a6b63173d4f4c87caf7257579ff339083ff38f13
Aske Ertmann <aske@moc.net>
2013-04-19 11:27:45 +0200

    [FEATURE] Add resource view helper for fetching file resources (FAL)
    Change-Id: I54bb6d870912977018bf110b39c3827b7d5738c2
    Refs: #11253

---
+++ Classes/ViewHelpers/ResourceViewHelper.php
@@ -0,0 +1,27 @@
+<?php
+namespace MOC\MocHelpers\ViewHelpers;
+
+class ResourceViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @var \TYPO3\CMS\Core\Resource\ResourceFactory
+	 */
+	protected $resourceFactory;
+
+	/**
+	 * @param \TYPO3\CMS\Core\Resource\ResourceFactory $resourceFactory
+	 * @return void
+	 */
+	public function injectFlexFormService(\TYPO3\CMS\Core\Resource\ResourceFactory $resourceFactory) {
+		$this->resourceFactory = $resourceFactory;
+	}
+
+	/**
+	 * @param string $identifier
+	 * @return \TYPO3\CMS\Core\Resource\FileInterface|\TYPO3\CMS\Core\Resource\Folder
+	 */
+	public function render($identifier = NULL) {
+		return $this->resourceFactory->retrieveFileOrFolderObject($identifier ?: $this->renderChildren());
+	}
+
+}
