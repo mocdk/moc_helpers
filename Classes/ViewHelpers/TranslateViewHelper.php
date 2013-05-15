683b8f9e1b6c5af7e944cf72ea2f9550cdd093ad
Lau Rosenvold <lau@moc.net>
2013-05-15 11:21:46 +0200

    [FEATURE] 6.1 update for TranslateViewHelper
    Updated TranslateViewHelper for TYPO3 6.1
    
    Change-Id: I881827c79155e11906756fb485ecc3eab5fbc387

--- Classes/ViewHelpers/TranslateViewHelper.php
+++ Classes/ViewHelpers/TranslateViewHelper.php
@@ -1,44 +1,37 @@
 <?php
 namespace MOC\MocHelpers\ViewHelpers;
 
-class TranslateViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\TranslateViewHelper {
+class TranslateViewHelper extends \TYPO3\CMS\Fluid\ViewHelpers\TranslateViewHelper {
 
 	/**
 	 * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface
+	 * @inject
 	 */
 	protected $configurationManager;
 
 	/**
-	 * @param \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager
-	 * @return void
-	 */
-	public function injectConfigurationManager(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface $configurationManager) {
-		$this->configurationManager = $configurationManager;
-	}
-
-	/**
 	 * Translate a given key or use the tag body as default.
 	 *
-	 * @param string $key The locallang key
-	 * @param string $default If the given locallang key could not be found, this value is used. If this argument is not set, child nodes will be used to render the default
-	 * @param boolean $htmlEscape TRUE if the result should be htmlescaped. This won't have an effect for the default value
-	 * @param array $arguments Arguments to be replaced in the resulting string
-	 * @param string $extensionName
+	 * @param string $id The locallang id
 	 * @return string The translated key or tag body if key doesn't exist
-	 * @author Christopher Hlubek <hlubek@networkteam.com>
-	 * @author Bastian Waidelich <bastian@typo3.org>
-	 * @author Aske Ertmann <aske@moc.net>
 	 */
-	public function render($key, $default = NULL, $htmlEscape = TRUE, array $arguments = NULL, $extensionName = '') {
-		if ($extensionName === '') {
-			$request = $this->controllerContext->getRequest();
+	protected function renderTranslation($id) {
+		$request = $this->controllerContext->getRequest();
+
+			// Set extension name to current settings if available
+		if ($this->arguments['extensionName'] === NULL) {
 			$settings = $this->configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS);
-			$extensionName = isset($settings['extensionName']) ? $settings['extensionName'] : $request->getControllerExtensionName();
+			$this->arguments['extensionName'] = isset($settings['extensionName']) ? $settings['extensionName'] : NULL;
 		}
-		$value = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate($key, $extensionName, $arguments);
+
+		$extensionName = $this->arguments['extensionName'] === NULL ? $request->getControllerExtensionName() : $this->arguments['extensionName'];
+		$value = \TYPO3\CMS\Extbase\Utility\LocalizationUtility::translate($id, $extensionName, $this->arguments['arguments']);
 		if ($value === NULL) {
-			$value = $default !== NULL ? $default : $this->renderChildren();
-		} elseif ($htmlEscape) {
+			$value = $this->arguments['default'] !== NULL ? $this->arguments['default'] : $this->renderChildren();
+			if (is_array($this->arguments['arguments'])) {
+				$value = vsprintf($value, $this->arguments['arguments']);
+			}
+		} elseif ($this->arguments['htmlEscape']) {
 			$value = htmlspecialchars($value);
 		}
 		return $value;
